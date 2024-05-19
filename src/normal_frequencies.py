83889823be390c744dff2e4acfcb89cdf577cb3f
51Sirius <71227522+51sirius@users.noreply.github.com>
2024-05-19 12:34:17 +0300

    Initial commit

---
+++ src/normal_frequencies.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def search_normal_frequencies(g, m, k, L, L1):
+    # Матрица коэффициентов
+    a = g / L + k / (m * L1)
+    b = -k / (m * L1)
+
+    A = np.array([[a, b], [b, a]])
+
+    # Поиск собственных значений (квадраты нормальных частот)
+    eigenvalues, eigenvectors = np.linalg.eig(A)
+
+    # Нормальные частоты
+    omega1 = np.sqrt(eigenvalues[0])
+    omega2 = np.sqrt(eigenvalues[1])
+
+    print(f'Нормальные частоты: ω1 = {omega1:.4f}, ω2 = {omega2:.4f}')
