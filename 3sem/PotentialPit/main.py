e115e1540c614b94b5908193299ac8f779da662b
51Sirius <71227522+51sirius@users.noreply.github.com71227522+51sirius@users.noreply.github.comm>
2024-12-20 09:29:56 +0300

    remake pits

--- 3sem/PotentialPit/main.py
+++ 3sem/PotentialPit/main.py
@@ -1,110 +1,40 @@
 import math
 import matplotlib.pyplot as plt
-from py_linq import Enumerable
-from scipy.optimize import fsolve
 import numpy as np
+from scipy.linalg import eigh
 from consts import *
 
-
-
-def intersect(x1, x2, y1, y2, eps):
-  points = []
-  for i in range(len(x1)):
-
-    dist = math.sqrt((x1[i] - x2[i]) ** 2 + (y1[i] - y2[i]) ** 2)
-    if dist <= eps:
-      points.append((x1[i], y1[i], dist))
-
-  points = Enumerable(points)
-  min_dist = points.select(lambda x: x[2]).min()
-  min_point = points.where(lambda x: x[2] == min_dist).first()
-
-  return (min_point[0], min_point[1])
-
-
-xMin = -a - a
-xMax = a + a
-x = np.arange(xMin, xMax, 1e-12)
 
 y = []
 
 for i in range(len(x)):
-    y.append(-U if abs(x[i]) <= a else 0)
+    y.append(-U0 if - a/2 <= x[i] <= a/2 else 0)
+k_2max = math.sqrt(2 * m * U0) / hbar
+n_max = math.ceil(k_2max * a / constants.pi)
+V = np.where(np.abs(x) < a / 2, -U0, 0)
 
-plt.plot(x, y)
-plt.xlabel("x, м")
-plt.ylabel("U(x), Дж")
+diag = hbar**2 / (2 * m * dx**2)
+
+H = np.diag(2 * diag + V) + np.diag(-diag * np.ones(N - 1), k=-1) + np.diag(-diag * np.ones(N - 1), k=1)
+
+self_state, self_vecs = eigh(H)
+engs = self_state / eV
+eng_vecs = self_vecs / np.sqrt(dx)
+
+linked_states_if = engs < 0
+linked_eng = engs[linked_states_if]
+linked_engvecs = eng_vecs[:, linked_states_if]
+
+
+plt.figure(figsize=(12, 8))
+for i in range (n_max):
+    plt.plot(x,linked_engvecs[:, i] / np.max(np.abs(linked_engvecs[:, i])) * 0.3 + linked_eng[i])
+
+plt.plot(x, V / eV, color='black')
+plt.xlabel("x, nm")
+plt.ylabel("E, eV")
+plt.ylim(-U0 / eV - 1, 1)
 plt.title("Potential Pit")
 plt.legend()
 plt.grid()
 plt.show()
-
-k_2 = math.sqrt(2 * m * U) / constants.hbar
-
-plt.figure(figsize=(10, 7))
-
-print(f"max k_2: {k_2}")
-
-lefty = np.arange(0, k_2, eps, dtype=float) * a
-leftx = np.arange(0, k_2, eps)
-n = math.ceil(k_2 * a / constants.pi)
-
-plt.plot(leftx, lefty)
-
-res = []
-
-for i in range(1, n + 1):
-    rightx = []
-    righty = []
-    for j in range(0, int(k_2), eps):
-        rightx.append(j)
-        righty.append(constants.pi * i - 2 * math.asin((constants.hbar * j) / math.sqrt(2 * constants.electron_mass * U)))
-
-    plt.plot(rightx, righty, color='red')
-    point = intersect(leftx, rightx, lefty, righty, eps)
-    plt.plot(point[0], point[1],'go', color='orange')
-    res.append((point[0], point[1]))
-
-
-for i in range(len(res)):
-    print(f'{res[i][0]:.3f} : {res[i][1]:.3f}')
-
-plt.xlabel("$k_2$")
-plt.ylabel(r"$f(k_2)$")
-plt.title(r"$k_2a = \pi n - 2\arcsin(\frac{\hbar k_2}{\sqrt{2mU}})$")
-plt.legend()
-plt.grid()
-plt.show()
-
-
-plt.figure(figsize=(10, 7))
-plt.plot(x, y)
-energy = [0] * len(res)
-
-def psi(x: float, n: int, scale: float = 1e23) -> float:
-    if abs(x) > a:
-        return 0
-    return math.sqrt(2 / a) * math.sin(constants.pi * (n + 1) * x / (2*a) + (n + 1) * constants.pi / 2) / scale
-
-for i in range(len(res)):
-    energy[i] = constants.hbar ** 2 * res[i][0] ** 2 / (2 * m)
-    plt.axhline(y=energy[i] - U, linestyle="--", linewidth = 0.5)
-
-    y_psi = []
-    x_psi = []
-    for j in range(len(x)):
-        tmp = psi(x[j], i)
-        if tmp != 0:
-          y_psi.append(tmp + (energy[i] - U))
-          x_psi.append(x[j])
-
-    plt.plot(x_psi, y_psi, color='red')
-
-plt.xlabel("x, м")
-plt.ylabel("U(x), Дж")
-plt.title("Potential Pit with Energy connect statements")
-plt.legend()
-plt.grid()
-plt.show()
-for i in energy:
-    print(f'{i:.3e}', end = " ")
